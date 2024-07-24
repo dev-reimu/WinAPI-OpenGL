0afa34c68b839d4b1e2fe692b06e1bde0eca6012
dev-reimu <github.543ov@slmails.com>
2024-07-24 17:10:10 +0300

    Add code that creates a window in Windows

---
+++ platforms/windows/create_window.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow);
