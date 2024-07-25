607ecca74218678244c2a934eba582aa86d3f67d
dev-reimu <github.543ov@slmails.com>
2024-07-25 22:07:10 +0300

    Tie OpenGL to windows/create_window.c

--- build_scripts/windows.bat
+++ build_scripts/windows.bat
@@ -1,3 +1,3 @@
 cls
-gcc ..\platforms\windows\create_window.c -o ..\executables\windows\windows.exe -municode
+gcc ..\platforms\windows\create_window.c -o ..\executables\windows\windows.exe -municode -lgdi32 -lopengl32
 ..\executables\windows\windows.exe
--- platforms/windows/create_window.c
+++ platforms/windows/create_window.c
@@ -6,7 +6,7 @@
 #endif
 
 #include <windows.h>
-#include <GL/GL.h>
+#include <GL/gl.h>
 
 #pragma comment (lib, "opengl32.lib")
 
@@ -14,12 +14,10 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 
     MSG msg          = {0};
-	WNDCLASSW wc      = {0}; 
+	WNDCLASSW wc     = {0};
 	wc.lpfnWndProc   = WindowProc;
 	wc.hInstance     = hInstance;
 	wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
@@ -63,28 +61,42 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
 
         case WM_CREATE: {
-                PIXELFORMATDESCRIPTOR pfd = {
-                    sizeof(PIXELFORMATDESCRIPTOR),
-                    1,
-                    PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
-                    PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
-                    32,                   // Colordepth of the framebuffer.
-                    0, 0, 0, 0, 0, 0,
-                    0,
-                    0,
-                    0,
-                    0, 0, 0, 0,
-                    24,                   // Number of bits for the depthbuffer
-                    8,                    // Number of bits for the stencilbuffer
-                    0,                    // Number of Aux buffers in the framebuffer.
-                    PFD_MAIN_PLANE,
-                    0,
-                    0, 0, 0
-                };
+            PIXELFORMATDESCRIPTOR pfd = {
+                sizeof(PIXELFORMATDESCRIPTOR),
+                1,
+                // Flags
+                PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, 
+                PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+                32,                   // Colordepth of the framebuffer.
+                0, 0, 0, 0, 0, 0,
+                0,
+                0,
+                0,
+                0, 0, 0, 0,
+                24,                   // Number of bits for the depthbuffer
+                8,                    // Number of bits for the stencilbuffer
+                0,                    // Number of Aux buffers in the framebuffer.
+                PFD_MAIN_PLANE,
+                0,
+                0, 0, 0
+            };
 
-                HDC ourWindowHandleToDeviceContext = GetDC(hwnd);
+            HDC ourWindowHandleToDeviceContext = GetDC(hwnd);
+
+            int letWindowsChooseThisPixelFormat;
+            letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd); 
+            SetPixelFormat(ourWindowHandleToDeviceContext,letWindowsChooseThisPixelFormat, &pfd);
+
+            HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+            wglMakeCurrent (ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+
+            MessageBoxA(0, (char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
+
+            wglMakeCurrent(ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+            // wglDeleteContext(ourOpenGLRenderingContext);
         }
-        return 0;
+
+        break;
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
