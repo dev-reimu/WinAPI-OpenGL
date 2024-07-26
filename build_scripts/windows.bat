d0aa533cbb2fdb393a1af3607fc6f4449066b6cb
dev-reimu <github.543ov@slmails.com>
2024-07-26 21:41:33 +0300

    Update windows.bat

--- build_scripts/windows.bat
+++ build_scripts/windows.bat
@@ -1,4 +1,4 @@
 mkdir ..\executables\windows
 cls
-gcc ..\source\windows\open_gl.c ..\source\windows\create_window.c -o ..\executables\windows\windows.exe -municode -lgdi32 -lopengl32
+gcc ..\source\windows\create_window.c ..\source\windows\open_gl.c -o ..\executables\windows\windows.exe -municode -lgdi32 -lopengl32
 ..\executables\windows\windows.exe
